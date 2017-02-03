4e6999979c82f6cc02f13aaaf9681e27d602b72f
John Gardiner <jwegardiner@gmail.com>
2017-02-03 13:12:42 +0000

    convert to es6

--- cli.js
+++ cli.js
@@ -6,16 +6,12 @@
  * ============
 **/
 
-var prc = require('yargs');
-var cmd = require('./cmd');
-var cwd = process.cwd();
+const prc = require('yargs')
+const cmd = require('./cmd')
+const cwd = process.cwd()
 
 prc.usage('$0 <cmd> [args]')
-       .command('watch', 
-                'watch development', 
-                function () {cmd.watch(cwd);})
-       .command('compile', 
-                'compile project and generate file cache', 
-                function () {cmd.compile(cwd);})                
-        .help()
-        .argv;
+   .command('watch', 'watch development', () => {cmd.watch(cwd)})
+   .command('compile', 'compile production', () => {cmd.compile(cwd)})                
+   .help()
+   .argv
