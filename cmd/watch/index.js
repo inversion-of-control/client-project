b0456b1ead235a6c0e5fb8c98db745594782beca
John Gardiner <john.gardiner@algomi.com>
2017-02-03 01:30:45 +0000

    use process.cwd()

--- cmd/watch/index.js
+++ cmd/watch/index.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var npmRun = require('npm-run');
 var path = require('path');
-var config;
+var exec, config;
 
 module.exports = function watch(cwd) {
+    exec = path.resolve(__dirname, '../../../../@inversion-of-control/client-project/node_modules/.bin/webpack-dev-server');
     config = path.resolve(__dirname, '../../lib/development.config.js'); 
-    npmRun.exec('webpack-dev-server --watch --config ' + config, {cwd: cwd}, onWatch); 
+    npmRun.exec(exec + ' --watch --config ' + config, {cwd: cwd}, onWatch); 
 };
 
 function onWatch(err, stdout, stderr) {
